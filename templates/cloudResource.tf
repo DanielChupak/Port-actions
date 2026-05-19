c713c1c2c3975e21bc60160f25c747772e694426
DanielChupak <danielc@port.io>
2026-05-19 11:58:10 +0300

    Update cloudResource.tf

--- templates/cloudResource.tf
+++ templates/cloudResource.tf
@@ -1,6 +1,10 @@
 resource "google_storage_bucket" "{{ bucket_name }}" {
   name                        = "{{ bucket_name }}"
   location                    = "EU"
-  uniform_bucket_level_access = false
-  predefined_acl              = "{{ bucket_acl }}"
+  uniform_bucket_level_access = true
+
+  dynamic "iam_configuration" {
+    for_each = []
+    content {}
+  }
 }
